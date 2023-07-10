4f0dfdf77753dc94e9248cb66972ebc4d57c0022
antoniomj <contatomoraesjr@gmail.com>
2023-07-10 17:36:24 -0300

    first commit with log middleware

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+
+@Module({
+  imports: [TodoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
